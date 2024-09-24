bf680cd7943cf21aebeefbf06e507e59f7c44993
dev-xero <elo48858@gmail.com>
2024-09-24 23:02:52 +0100

    solve: Problem 112 path sum

---
+++ dfs/path_sum.py
@@ -0,0 +1,36 @@
+# https://leetcode.com/problems/path-sum
+
+"""
+Given the root of a binary tree and an integer targetSum, return true if the tree has a root-to-leaf path such that adding up all the values along the path equals targetSum.
+
+A leaf is a node with no children.
+"""
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        global found
+        found = False
+
+        def dfs(root, depth_sum):
+            if not root:
+                return False
+            
+            depth_sum += root.val
+
+            if not root.left and not root.right and depth_sum == targetSum:
+                global found
+                found = True
+                return
+
+            dfs(root.left, depth_sum)
+            dfs(root.right, depth_sum)
+        
+        dfs(root, 0)
+        return found
+        
