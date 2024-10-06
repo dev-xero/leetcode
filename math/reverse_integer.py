8bfb6d98085ff69e1953edbd2cac6d29a7f5c795
dev-xero <elo48858@gmail.com>
2024-10-06 21:46:32 +0100

    solve: Problem 7 reverse 32-bit integer

---
+++ math/reverse_integer.py
@@ -0,0 +1,28 @@
+# https://leetcode.com/problems/reverse-integer
+
+
+"""
+Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+
+Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+"""
+
+
+class Solution:
+    def reverse(self, x: int) -> int:
+        MIN, MAX = -(2**31), 2**31 - 1
+        res = 0
+
+        sign = 1 if x > 0 else -1
+        x = abs(x)
+
+        while x != 0:
+            q = int(x % 10)
+
+            if (res > MAX // 10) or (res == MAX // 10 and q > 7):
+                return 0
+
+            res = res * 10 + q
+            x //= 10
+
+        return sign * res
