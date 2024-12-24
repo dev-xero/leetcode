8f9502e33863ad11209cdc8a8bd9870c27776b79
= <ssh.xero@gmail.com>
2024-12-24 16:55:05 +0100

    solve: Problem 1475 final prices with special discount in a shop

---
+++ array/final_prices_with_special_discount_in_a_shop.py
@@ -0,0 +1,19 @@
+# https://leetcode.com/problems/final-prices-with-a-special-discount-in-a-shop
+
+"""
+You are given an integer array prices where prices[i] is the price of the ith item in a shop.
+
+There is a special discount for items in the shop. If you buy the ith item, then you will receive a discount equivalent to prices[j] where j is the minimum index such that j > i and prices[j] <= prices[i]. Otherwise, you will not receive any discount at all.
+
+Return an integer array answer where answer[i] is the final price you will pay for the ith item of the shop, considering the special discount.
+"""
+
+class Solution:
+    def finalPrices(self, prices: List[int]) -> List[int]:
+        for i in range(len(prices)):
+            for j in range(i+1, len(prices)):
+                if prices[j] <= prices[i]:
+                    prices[i] = prices[i] - prices[j]
+                    break
+
+        return prices
