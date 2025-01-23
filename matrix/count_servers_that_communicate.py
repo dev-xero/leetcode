4507a6956cf70795eba31a1fdfa13d19e6ccdc3e
= <ssh.xero@gmail.com>
2025-01-23 20:03:05 +0100

    solve: Problem 1267 count servers that communicate

---
+++ matrix/count_servers_that_communicate.py
@@ -0,0 +1,29 @@
+# https://leetcode.com/problems/count-servers-that-communicate/description/?envType=daily-question&envId=2025-01-23
+
+"""
+You are given a map of a server center, represented as a m * n integer matrix grid, where 1 means that on that cell there is a server and 0 means that it is no server. Two servers are said to communicate if they are on the same row or on the same column.
+
+Return the number of servers that communicate with any other server.
+"""
+
+
+class Solution:
+    def countServers(self, grid: List[List[int]]) -> int:
+        m, n = len(grid), len(grid[0])
+
+        row_count = [0] * m
+        col_count = [0] * n
+
+        for row in range(m):
+            for col in range(n):
+                if grid[row][col]:
+                    row_count[row] += 1
+                    col_count[col] += 1
+
+        count = 0
+        for row in range(m):
+            for col in range(n):
+                if grid[row][col] and (row_count[row] > 1 or col_count[col] > 1):
+                    count += 1
+
+        return count
