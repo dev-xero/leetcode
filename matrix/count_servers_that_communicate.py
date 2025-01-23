d39fe5eacf0027f12deb782b34f82ed0633baf2f
= <ssh.xero@gmail.com>
2025-01-23 20:06:54 +0100

    solve: Problem 1267 count servers that communicate

--- matrix/count_servers_that_communicate.py
+++ matrix/count_servers_that_communicate.py
@@ -1,4 +1,4 @@
-# https://leetcode.com/problems/count-servers-that-communicate/description/?envType=daily-question&envId=2025-01-23
+# https://leetcode.com/problems/count-servers-that-communicate
 
 """
 You are given a map of a server center, represented as a m * n integer matrix grid, where 1 means that on that cell there is a server and 0 means that it is no server. Two servers are said to communicate if they are on the same row or on the same column.
