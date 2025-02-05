c6d6200687c8a85e626c134f8e45aae998edf732
= <ssh.xero@gmail.com>
2025-02-05 11:08:46 +0100

    solve(array): Problem 3379 - Transformed array

---
+++ array/transformed_array.py
@@ -0,0 +1,32 @@
+# 
+
+class Solution:
+    def constructTransformedArray(self, nums: List[int]) -> List[int]:
+        """
+            You are given an integer array nums that represents a circular array. Your task is to create a new array result of the same size, following these rules:
+            For each index i (where 0 <= i < nums.length), perform the following independent actions:
+
+                If nums[i] > 0: Start at index i and move nums[i] steps to the right in the circular array. Set result[i] to the value of the index where you land.
+                If nums[i] < 0: Start at index i and move abs(nums[i]) steps to the left in the circular array. Set result[i] to the value of the index where you land.
+                If nums[i] == 0: Set result[i] to nums[i].
+
+            Return the new array result.
+
+            Note: Since nums is circular, moving past the last element wraps around to the beginning, and moving before the first element wraps back to the end.
+        """
+
+        N = len(nums)
+        result = [0] * N
+        for i in range(N):
+            if nums[i] > 0:
+                nxt = nums[(i + nums[i]) % N]
+                result[i] = nxt
+            
+            elif nums[i] < 0:
+                nxt = nums[(i - abs(nums[i])) % N]
+                result[i] = nxt
+            
+            else:
+                result[i] = nums[i]
+
+        return result
